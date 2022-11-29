209a24b2308729a16035119fa5848cc2b8c746ea
Gage <yaujin@protonmail.com>
2022-11-28 19:25:57 -0600

    made event listener for password element to match confirmPassword. So content will be updated if user updates either field

--- javascript.js
+++ javascript.js
@@ -4,6 +4,21 @@ const passMatchSpan = document.querySelector(".pass-match-span");
 const password = document.getElementById("pass");
 const confirmPassword = document.getElementById("pass-confirm");
 
+password.addEventListener("keyup", () => {
+  if (
+    password.checkValidity() === true &&
+    confirmPassword.checkValidity() === true
+  ) {
+    const passwordValue = document.getElementById("pass").value;
+    const confirmPasswordValue = document.getElementById("pass-confirm").value;
+    if (passwordValue === confirmPasswordValue) {
+      passMatchSpan.hidden = true;
+    } else {
+      passMatchSpan.hidden = false;
+    }
+  }
+});
+
 confirmPassword.addEventListener("keyup", () => {
   if (
     password.checkValidity() === true &&
