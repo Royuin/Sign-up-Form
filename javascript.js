2e58f74c881211edcc8db5655a03a70937d2d667
Gage <yaujin@protonmail.com>
2022-11-28 18:03:07 -0600

    made selectors for password and confirmPassword

--- javascript.js
+++ javascript.js
@@ -1,2 +1,5 @@
 const submitBtn = document.querySelector(".submit-btn");
 const passMatchSpan = document.querySelector(".pass-match-span");
+
+let password = document.getElementById("pass");
+let confirmPassword = document.getElementById("pass-confirm");
