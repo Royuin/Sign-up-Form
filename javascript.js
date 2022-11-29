a15fd5cf549d3b211f1fc426f3a99f33c7ab789e
Gage <yaujin@protonmail.com>
2022-11-28 19:10:07 -0600

    made event listener for confirmPassword with if statement to check if both password fields are valid

--- javascript.js
+++ javascript.js
@@ -3,3 +3,11 @@ const passMatchSpan = document.querySelector(".pass-match-span");
 
 let password = document.getElementById("pass");
 let confirmPassword = document.getElementById("pass-confirm");
+
+confirmPassword.addEventListener("keyup", () => {
+  if (
+    password.checkValidity() === true &&
+    confirmPassword.checkValidity() === true
+  ) {
+  }
+});
