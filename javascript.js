ee3dc070b04e9aa39434b6b5f9efa1a16940acd7
Gage <yaujin@protonmail.com>
2022-11-28 19:23:06 -0600

    changed declaration of password and confirmPassword from let to const

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 const submitBtn = document.querySelector(".submit-btn");
 const passMatchSpan = document.querySelector(".pass-match-span");
 
-let password = document.getElementById("pass");
-let confirmPassword = document.getElementById("pass-confirm");
+const password = document.getElementById("pass");
+const confirmPassword = document.getElementById("pass-confirm");
 
 confirmPassword.addEventListener("keyup", () => {
   if (
