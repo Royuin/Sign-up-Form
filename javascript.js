742fb26ca0d6183456f8cc216a18b6419ca8c45b
Gage <yaujin@protonmail.com>
2022-11-29 18:48:53 -0600

    removed if statements at start of both password event listeners that checked if passwords were valid. Event listeners would not update passwords to valid if one of them became invalid

--- javascript.js
+++ javascript.js
@@ -5,39 +5,29 @@ const password = document.getElementById("pass");
 const confirmPassword = document.getElementById("pass-confirm");
 
 password.addEventListener("keyup", () => {
-  if (
-    password.checkValidity() === true &&
-    confirmPassword.checkValidity() === true
-  ) {
-    const passwordValue = document.getElementById("pass").value;
-    const confirmPasswordValue = document.getElementById("pass-confirm").value;
-    if (passwordValue === confirmPasswordValue) {
-      passMatchSpan.hidden = true;
-      password.setCustomValidity("");
-      confirmPassword.setCustomValidity("");
-    } else {
-      passMatchSpan.hidden = false;
-      password.setCustomValidity("Passwords do not match");
-      confirmPassword.setCustomValidity("Passwords do not match");
-    }
+  const passwordValue = document.getElementById("pass").value;
+  const confirmPasswordValue = document.getElementById("pass-confirm").value;
+  if (passwordValue === confirmPasswordValue) {
+    passMatchSpan.hidden = true;
+    password.setCustomValidity("");
+    confirmPassword.setCustomValidity("");
+  } else {
+    passMatchSpan.hidden = false;
+    password.setCustomValidity("Passwords do not match");
+    confirmPassword.setCustomValidity("Passwords do not match");
   }
 });
 
 confirmPassword.addEventListener("keyup", () => {
-  if (
-    password.checkValidity() === true &&
-    confirmPassword.checkValidity() === true
-  ) {
-    const passwordValue = document.getElementById("pass").value;
-    const confirmPasswordValue = document.getElementById("pass-confirm").value;
-    if (passwordValue === confirmPasswordValue) {
-      passMatchSpan.hidden = true;
-      password.setCustomValidity("");
-      confirmPassword.setCustomValidity("");
-    } else {
-      passMatchSpan.hidden = false;
-      password.setCustomValidity("Passwords do not match");
-      confirmPassword.setCustomValidity("Passwords do not match");
-    }
+  const passwordValue = document.getElementById("pass").value;
+  const confirmPasswordValue = document.getElementById("pass-confirm").value;
+  if (passwordValue === confirmPasswordValue) {
+    passMatchSpan.hidden = true;
+    password.setCustomValidity("");
+    confirmPassword.setCustomValidity("");
+  } else {
+    passMatchSpan.hidden = false;
+    password.setCustomValidity("Passwords do not match");
+    confirmPassword.setCustomValidity("Passwords do not match");
   }
 });
